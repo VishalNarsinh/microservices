dfb17f3417de0ff6d9a3b202a29791cf9fb680dd
Vishal Narsinh <vishal@tryspeed.com>
2025-08-29 16:17:40 +0530

    Add: Docker Compose file

--- accounts/src/main/java/com/vishal/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/vishal/accounts/controller/AccountController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/",produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 @Validated
+@Slf4j
 public class AccountController {
 
     IAccountsService accountsService;
